a233173d2f4ad2d386e19adae511142daaf7708e
Joshua Chen <joshuavictorchen@gmail.com>
2025-05-26 11:44:47 -0400

    bump up grid workers

--- source/utils.py
+++ source/utils.py
@@ -1,7 +1,7 @@
 MIN_INSTRUCTOR_PER_HEAT = 3  # this is modified in roles_and_minima()
 MIN_TIMING_PER_HEAT = 2
 MIN_START_PER_HEAT = 1
-MIN_GRID_PER_HEAT = 1
+MIN_GRID_PER_HEAT = 2
 
 
 def parse_bool(value):
