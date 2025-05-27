eb308cd79815af29b0dd572acc2bce86b06db284
Joshua Chen <joshuavictorchen@gmail.com>
2025-05-27 19:31:30 -0400

    pedantic changes + member ID correction

--- source/Event.py
+++ source/Event.py
@@ -16,13 +16,13 @@ class Event(Group):
 
     def __init__(
         self,
-        csv_file: str,
-        member_ability_csv_file: str,
+        msr_export_csv: str,
+        member_attributes_csv: str,
         number_of_heats: int,
         number_of_stations: int,
     ):
         self.number_of_stations = number_of_stations
-        self.participants = self.load_participants(csv_file, member_ability_csv_file)
+        self.participants = self.load_participants(msr_export_csv, member_attributes_csv)
         self.categories = self.load_categories()
         self.heats = self.load_heats(number_of_heats)
 
@@ -39,32 +39,34 @@ class Event(Group):
             max_length = len(p.name) if len(p.name) > max_length else max_length
         return max_length
 
-    def load_participants(self, csv_file: str, member_ability_csv_file: str):
+    def load_participants(self, msr_export_csv: str, member_attributes_csv: str):
         """
-        Loads participants from an MSR Exported CSV file, then gets their possible work assignments from the private Member CSV file.
+        Loads participants from `msr_export_csv`, then gets their possible work assignments from `member_attributes_csv`.
+
+        TODO: currently requires the CSVs to match the samples exactly, with case sensitivity; loosen these shackles.
 
         Returns:
             list[Participant]: All parsed participants.
         """
-        member_ability_dictionary = {}
-        with open(member_ability_csv_file, newline="", encoding="utf-8") as member_ability_csv:
-            member_data = csv.DictReader(member_ability_csv)
-            for _id, row in enumerate(member_data):
-                member_ability_dictionary[row["id"]] = row
+        member_attributes_dict = {}
+        with open(member_attributes_csv, newline="", encoding="utf-8-sig") as file:
+            member_data = csv.DictReader(file)
+            for row in member_data:
+                member_attributes_dict[row["id"]] = row
 
         participants = []
-        with open(csv_file, newline="", encoding="utf-8-sig") as file:
+        with open(msr_export_csv, newline="", encoding="utf-8-sig") as file:
             reader = csv.DictReader(file)
-            for i, row in enumerate(reader):
-                member_ability_data = member_ability_dictionary.get(utils.get_formatted_member_number(row["Member #"]), {})
+            for row in reader:
+                member_attributes = member_attributes_dict.get(utils.get_formatted_member_number(row["Member #"]))
                 participant = Participant(
                     event=self,
-                    id=i,
+                    id=row["id"],
                     name=row["Name"],
                     category_string=row["Class"] if row["Modifier"] in ["", "NOV"] else row["Modifier"],
                     novice=utils.parse_bool(row["Modifier"] == "NOV"),
                     **{
-                        role: utils.parse_bool(member_ability_data.get(role))
+                        role: utils.parse_bool(member_attributes.get(role))
                         for role in utils.roles_and_minima(
                             number_of_stations=self.number_of_stations
                         )
